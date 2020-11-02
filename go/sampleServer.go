0d0d848fdad87b72278811ed195f9ed14d3b8c18
farhadi-erfan <farhadi.erfan76@gmail.com>
2020-11-02 08:59:20 +0330

    added error handling to go

--- go/sampleServer.go
+++ go/sampleServer.go
@@ -14,6 +14,8 @@ import (
 	"io/ioutil"
 )
 
+var lines []string
+
 type nums struct {
 	N1 int `json:"n1"`
 	N2 int `json:"n2"`
@@ -29,23 +31,14 @@ func handlerSha(w http.ResponseWriter, r *http.Request) {
 	var data nums
 	err := decoder.Decode(&data)
 	if err != nil {
-		panic(err)
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid numbers")
+	} else {
+		n1 := data.N1
+		n2 := data.N2
+		var sum = hashIt(n1, n2)
+		fmt.Fprintf(w, "%s", sum)
 	}
-	n1 := data.N1
-	n2 := data.N2
-	var sum = hashIt(n1, n2)
-    fmt.Fprintf(w, "%s", sum)
-}
-
-func handlerWrite(w http.ResponseWriter, r *http.Request) {
-	content, _ := ioutil.ReadFile("../text.txt")
-	text := string(content)
-	lines := strings.Split(text, "\n")
-	query := r.URL.Query()
-    lineNumStr := query.Get("l")
-	lineNum, _ := strconv.Atoi(lineNumStr)
-	result := lines[lineNum]
-	fmt.Fprintf(w, "%s", result)
 }
 
 func hashIt(n1 int, n2 int) []byte {
@@ -60,8 +53,26 @@ func hashIt(n1 int, n2 int) []byte {
 	return res
 }
 
-func main() {
+func handlerWrite(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+    lineNumStr := query.Get("l")
+	lineNum, err := strconv.Atoi(lineNumStr)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "invalid number")
+	} else if (lineNum < 1 || lineNum > 100) {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "out of range")
+	} else {
+		result := lines[lineNum]
+		fmt.Fprintf(w, "%s", result)
+	}
+}
 
+func main() {
+	content, _ := ioutil.ReadFile("../text.txt")
+	text := string(content)
+	lines = strings.Split(text, "\n")
     http.HandleFunc("/go/sha256", handlerSha)
 	http.HandleFunc("/go/write", handlerWrite)
     log.Fatal(http.ListenAndServe(":8080", nil))
