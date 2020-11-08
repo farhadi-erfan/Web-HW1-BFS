de1c8bbe2504697341559fc5e049cf3d15c6be6c
farhadi-erfan <farhadi.erfan76@gmail.com>
2020-11-08 16:37:49 +0330

    added locust

--- nginx locust/test.py
+++ nginx locust/test.py
@@ -11,9 +11,9 @@ class QuickstartUser(HttpUser):
         self.client.get("/go/write?l={}".format(random.randint(1, 100)))
         self.client.get("/nodejs/write?l={}".format(random.randint(1, 100)))
 
-    #@task
-    #def sha(self):
-    #    n1, n2 = random.randint(1, 50000), random.randint(1, 50000)
-    #    self.client.post("/go/sha256", json={"n1": n1, "n2": n2}, headers=h)
-    #    self.client.post("/nodejs/sha256", json={"n1": n2, "n2": n1}, headers=h)
-    #    time.sleep(1)
+    @task
+    def sha(self):
+        n1, n2 = random.randint(1, 50000), random.randint(1, 50000)
+        self.client.post("/go/sha256", json={"n1": n1, "n2": n2}, headers=h)
+        self.client.post("/nodejs/sha256", json={"n1": n2, "n2": n1}, headers=h)
+        time.sleep(1)
